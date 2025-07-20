09d176e149fb2634b4e45d012df552c28a97ca5b
Prezzworld <japhetprecious14@gmail.com>
2025-07-20 09:07:45 -1100

    still checking for more oberflows

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const linkShow = document.querySelector('.nav-links');
 const icon = document.querySelector('.icon');
 const bars = document.querySelector('.fa-bars');
 
-icon.addEventListener('click', () => { 
-  linkShow.classList.toggle('show');
-  bars.classList.toggle('fa-xmark');
-});
+// icon.addEventListener('click', () => { 
+//   linkShow.classList.toggle('show');
+//   bars.classList.toggle('fa-xmark');
+// });
 
